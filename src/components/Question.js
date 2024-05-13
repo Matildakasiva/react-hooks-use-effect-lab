c89c121aa534cc8b190d8d138511870716bc85f2
Matilda <ogarimatilda@gmail.com>
2024-05-13 14:28:12 +0300

    completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  //"decrements the timer by 1 every second"
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000)
+  
+    return () => clearInterval(timer);
+  }, [])
+
+  //"calls onAnswered after 10 seconds"
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered])
+
+  //"clears the timeout after unmount"
+  useEffect (() => {
+    const timeoutId = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime -1)
+    }, 1000)
+    return () => clearTimeout(timeoutId)
+  }, [])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
